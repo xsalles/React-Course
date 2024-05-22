7ca79860b7d1547941e986f332193a335cba48f7
xsalles <xs.salles@gmail.com>
2024-05-21 21:12:18 -0300

    feat: added team to assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import "./App.css"
-import "./components/Layout/index"
 
-import Layout from "./components/Layout";
+
+import Content from "./components/Layout/Team/components/content"
+
 
 function App() {
   return (
     <body>
       
-     <Layout 
-     tag="react"
-     title="Curso Senai"
-     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Duis cursus, mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam libero vitae erat."
-     buttonLeftTitle="Leia Mais"
-     buttonRightTitle="Documentação"
-     image
-     />
+     <Content />
+     
+
     </body>
   );
 }
