76791b88e69abeae618849589b5f2bff6cd40c39
xsalles <xs.salles@gmail.com>
2024-05-22 19:09:44 -0300

    feat: index.js Composition

---
+++ src/components/Team/pattern/composition/index.js
@@ -0,0 +1,9 @@
+import Content from "./team-content.composition";
+import Main from "./team-main.composition";
+import Section from "./team-section.composition";
+
+export const Team = {
+    Content : Content,
+    Main : Main,
+    Section : Section
+}
