bf1d70eba950e5b5f1c336debf5aa60ce03a0525
xsalles <xs.salles@gmail.com>
2024-06-05 15:43:50 -0300

    feat: button submit

--- src/styles/css.modules/components/Blog/index.jsx
+++ src/styles/css.modules/components/Blog/index.jsx
@@ -1,30 +1,49 @@
-import styles from "./styles.module.css"
+import styles from "./styles.module.css";
 
-export default function Blog(){
-    return(
-        <form className={styles.blog}>
-            <div className={styles.login}>
-                <h1 className={styles.title}>Contact Us</h1>
-                <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
-                <div className={styles.forms}>
-                    <p className={styles.name}></p>
-                    <label htmlFor="Name">Name</label>
-                    <input type="text" name="Name" id="Name" className={styles.textArea}/>
-                    <p className={styles.email}></p>
-                    <label htmlFor="Email">Email</label>
-                    <input type="text" name="Email" id="Email" className={styles.textArea}/>
-                    <p className={styles.message}></p>
-                    <label htmlFor="Message">Message</label>
-                    <input placeholder="Type your message..." type="text" name="Message" id="Message" className={styles.textAreaMessage}/>
-                    <div className={styles.check}>
-                    <input type="checkBox" id="I accept the Terms" name="I accept the Terms" className={styles.checkBox}/>
-                    <label htmlFor="I accept the Terms" className={styles.terms}>I accept the Terms</label>
-                    <input type={styles.submit} />
-                    </div>
-                    
-                </div>
-                
-            </div>
+export default function Blog() {
+  return (
+    <form className={styles.blog}>
+      <div className={styles.login}>
+        <h1 className={styles.title}>Contact Us</h1>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+        </p>
+        <form className={styles.forms}>
+          <p className={styles.name}></p>
+          <label htmlFor="Name">Name</label>
+          <input
+            type="text"
+            name="Name"
+            id="Name"
+            className={styles.textArea}
+          />
+          <p className={styles.email}></p>
+          <label htmlFor="Email">Email</label>
+          <input
+            type="text"
+            name="Email"
+            id="Email"
+            className={styles.textArea}
+          />
+          <p className={styles.message}></p>
+          <label htmlFor="Message">Message</label>
+          <textarea
+            placeholder="Type your message..."
+            name="Message"
+            id="Message"
+            className={styles.textAreaMessage}
+          ></textarea>
+          <div className={styles.check}>
+            <input type="checkBox" id="checkbox" className={styles.checkBox} />
+            <p for="checkbox" className={styles.terms}>I accept the Terms </p>
+          </div>
+          <div className="button-container">
+            <a href="#" className={styles.button}>
+              Submit
+            </a>
+          </div>
         </form>
-    )
+      </div>
+    </form>
+  );
 }
