b6c19778ab8d2251e08592b908988d852b8bf404
xsalles <xs.salles@gmail.com>
2024-06-18 19:30:59 -0300

    refactor: styles of api

--- src/api/api-pokemon/index.jsx
+++ src/api/api-pokemon/index.jsx
@@ -18,13 +18,15 @@ export default function GetPokemon() {
     }
     
     return(
-        <div className="flex flex-col gap-10">
-            <h1 className="mt-2 text-center">Lista de Pokemons</h1>
-            <input placeholder="Digite o nome do pokemon" className="w-96 p-2 border-1 border-solid border-black-800" onChange={(e) => setPokemon(e.target.value)} type="text" />
-            <button onClick={handlePokemon} className="p-1 border-2 border-solid border-black-800 items-center justify-center">Buscar Pokemon</button>
+        <div className="flex items-center justify-center gap-5">
+            <div className="flex items-center justify-center flex-col gap-5">
+            <h1 >Lista de Pokemons</h1>
+            <input placeholder="Digite o nome do pokemon" className="p-2 border-solid border-indigo-500 rounded-md" onChange={(e) => setPokemon(e.target.value)} type="text" />
+            <button onClick={handlePokemon} className="flex items-center justify-center w-14 bg-indigo-500 rounded-md p-2 text-white">Buscar Pokemon</button>
+            </div>
             {
                 pokedex &&
-                        <div className="flex flex-col border-solid w-50 h-50">
+                        <div className="mb-3 shadow-2xl shadow-gray-600 mt-11 rounded-lg p-4 flex flex-col items-center justify-center w-20 h-30 gap-3">
                             <p className="text-center">{pokedex.name}:</p>
                             <img src={pokedex.sprites.front_default} alt="" />
                             <p className="text-center">shiny:</p>
