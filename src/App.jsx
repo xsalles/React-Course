11e1ea89d68a1609668817e6b0dac04c68804dd5
xsalles <xs.salles@gmail.com>
2024-06-19 20:52:38 -0300

    feat: added mui lib and feat login page with mui components

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
 import "./App.css"
-import DropdownMenu from "./hooks/components/DropDownMenu";
-import Faq from "./hooks/components/Faq";
-import Navbar from "./hooks/components/Navbar";
-import { Routes, Route} from "react-router-dom";
+
 import React from "react";
-import TeamComposition from "../src/components/Team";
-import Home from "./styles/tailwindcss/components/Home";
-import Contact from "./styles/css.modules/components/Contact";
-import GetCep from "./api/api-cep";
-import GetPokemon from "./api/api-pokemon/index";
+
+
+import Nav from "./mui/components/MainPage/nav";
+import SignIn from "./mui/components/MainPage/sign";
+import Footer from "./mui/components/MainPage/footer";
+
+
 
 
 function App() {
   return (
     <body>
-      <Routes >
-        <Route path="/" Component={GetPokemon} />
-        <Route path="/navbar" Component={Navbar} />
-        <Route path="/teams" Component={TeamComposition} />
-        <Route path="/contact" Component={Contact}/>
-        <Route path="/faq" Component={Faq} />
-        <Route path=""/>
-      </Routes >
+       <Nav />
+       <SignIn />
+       <Footer />
     </body>
   );
 }
--- src/styles/styled-component/components/Cta/cta.test.js
+++ src/styles/styled-component/components/Cta/cta.test.js
@@ -6,7 +6,6 @@ import '@testing-library/jest-dom';
 describe('I will do some tests with Cta', () => {
     it('verify width of cta', () => {
         const { container } = render(<Cta />)
-
         expect(container.firstChild).toHaveStyle('width: 1440px')
     })
 }) 
